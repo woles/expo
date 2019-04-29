99234a00116889b41ba0503997f1dd6b182785f8
szymon <szymon.kapala@swmansion.com>
2019-04-29 12:24:34 +0200

    fix notification.ts

--- packages/expo/src/Notifications/Notifications.ts
+++ packages/expo/src/Notifications/Notifications.ts
@@ -454,10 +454,7 @@ export default {
       repeat?: boolean;
     } = {}
   ): Promise<string> {
-    if (!ExponentNotifications.scheduleNotificationWithCalendar) {
-      return;
-    }
-    return ExponentNotifications.scheduleNotificationWithCalendar(nativeNotification, options);
+    return ExponentNotifications.scheduleNotificationWithCalendar(notification, options);
   },
 
   async schedulNotificationWithTimerAsync(  notification: LocalNotification,
@@ -466,9 +463,6 @@ export default {
       repeat?: boolean;
     } = {}
   ): Promise<string> {
-    if (!ExponentNotifications.schedulNotificationWithTimer) {
-      return;
-    }
-    return ExponentNotifications.scheduleNotificationWithTimer(nativeNotification, options);
+    return ExponentNotifications.scheduleNotificationWithTimer(notification, options);
   },
 };
