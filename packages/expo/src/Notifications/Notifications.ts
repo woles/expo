0a9f853ef75e8981e37eb0e18ff81941a3d98dba
szymon <szymon.kapala@swmansion.com>
2019-05-15 12:39:09 +0200

    several fixes

--- packages/expo/src/Notifications/Notifications.ts
+++ packages/expo/src/Notifications/Notifications.ts
@@ -459,7 +459,7 @@ export default {
 
   async scheduleNotificationWithTimerAsync(  notification: LocalNotification,
     options: {
-      interval?: number;
+      interval: number;
       repeat?: boolean;
     } = {}
   ): Promise<string> {
